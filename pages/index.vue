f6f93ef055015941c54d2f24db92454d4b3fa8f9
ekirian <ekidoseven@gmail.com>
2023-11-29 20:52:23 +0800

    📍checkpoint before adding firebase

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,26 @@
 
-
-<template>
-  <div class="p-4 mb-32 md:pr-8">
-    <!--  Time  -->
-    <div class="md:flex justify-between items-center">
-      <div class="text-center md:text-left">
-        <time-sect/>
-      </div>
-      <div class="md:flex items-center hidden">
-        <span>Hujan ringan</span>
-        <img src="https://cdn2.iconfinder.com/data/icons/weather-365/64/weather-sun-cloud-rain-512.png" alt="" class="w-16 ml-4">
-      </div>
-    </div>
-
-
-    <!--    content Wraper -->
-    <div class="flex flex-col">
-      <div class="md:flex flex-row md:space-x-4 mt-8">
-        <!--  Temperature Card-->
-        <div class="md:basis-1/4 bg-white rounded-3xl overflow-hidden">
-          <temp-card temp="32.3"
-                     update="3m"
-                     time="05:09 PM"
-                     change="0.4"
-                     humidity="80"
-                     high="34.1"
-                     highTime="2m"
-                     low="29.9"
-                     lowTime="6h"
-          />
-        </div>
-
-        <!--  Graph view-->
-        <div class="md:basis-3/4 md:m-0 mt-4 p-4 bg-white rounded-3xl md:block">
-          <div class="h-full">
-            <Line :data="chartData" :options="chartOptions" />
-          </div>
-        </div>
-
-      </div>
-
-      <div class="mt-4 p-4 bg-white rounded-3xl hidden md:block">
-        <calculation-card/>
-      </div>
-    </div>
-
-
-  </div>
-
-
-</template>
-
 <script lang="ts" setup>
 import { Line } from 'vue-chartjs';
-import { ref } from 'vue';
-// import { months, numbers } from '~/assets/js/chartjs-utils';
 
 interface ChartData {
   x: number;
   y: number;
 }
 
-const data: ChartData[] = [];
-const data2: ChartData[] = [];
+const tempData: ChartData[] = [];
+const humidData: ChartData[] = [];
 let prev = 29;
 let prev2 = 80;
 
 for (let i = 0; i < 100; i++) {
   prev += 5 - Math.random() * 10;
-  data.push({ x: i, y: prev });
+  tempData.push({ x: i, y: prev });
   prev2 += 5 - Math.random() * 10;
-  data2.push({ x: i, y: prev2 });
+  humidData.push({ x: i, y: prev2 });
 }
 
 // const labels = months({ count: 7 });
+// console.log(labels)
 
 const chartData = ref({
   // labels: labels,
@@ -84,7 +31,7 @@ const chartData = ref({
       borderWidth: 2,
       radius: 0,
       tension: 0.3,
-      data: data,
+      data: tempData,
       yAxisID: 'y',
     },
     {
@@ -93,14 +40,14 @@ const chartData = ref({
       borderWidth: 2,
       radius: 0,
       tension: 0.3,
-      data: data2,
+      data: humidData,
       yAxisID: 'y1',
     },
   ],
 });
 
 const totalDuration = 500;
-const delayBetweenPoints = totalDuration / data.length;
+const delayBetweenPoints = totalDuration / tempData.length;
 
 const previousY = (ctx: any) =>
     ctx.index === 0
@@ -193,3 +140,55 @@ const chartOptions = ref({
 });
 
 </script>
+
+<template>
+  <div class="p-4 mb-32 md:pr-8">
+    <!--  Time  -->
+    <div class="md:flex justify-between items-center">
+      <div class="text-center md:text-left">
+        <time-sect/>
+      </div>
+      <div class="md:flex items-center hidden">
+        <span>Hujan ringan</span>
+        <img src="https://cdn2.iconfinder.com/data/icons/weather-365/64/weather-sun-cloud-rain-512.png" alt="" class="w-16 ml-4">
+      </div>
+    </div>
+
+<!--    <p>{{cdata}}</p>-->
+
+    <!--    content Wraper -->
+    <div class="flex flex-col">
+      <div class="md:flex flex-row md:space-x-4 mt-8">
+        <!--  Temperature Card-->
+        <div class="md:basis-1/4 bg-white rounded-3xl overflow-hidden">
+          <temp-card temp="32.3"
+                     update="3m"
+                     time="05:09 PM"
+                     change="0.4"
+                     humidity="80"
+                     high="34.1"
+                     highTime="2m"
+                     low="29.9"
+                     lowTime="6h"
+          />
+        </div>
+
+        <!--  Graph view-->
+        <div class="md:basis-3/4 md:m-0 mt-4 p-4 bg-white rounded-3xl md:block">
+          <div class="h-full">
+            <Line :data="chartData" :options="chartOptions" />
+          </div>
+        </div>
+
+      </div>
+
+      <div class="mt-4 p-4 bg-white rounded-3xl hidden md:block">
+        <calculation-card/>
+      </div>
+    </div>
+
+
+  </div>
+
+
+</template>
