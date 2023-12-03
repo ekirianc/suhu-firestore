6a0c8a033ea36b568484ed1b4b8bf3e183b6608c
ekirian <ekidoseven@gmail.com>
2023-12-03 23:28:21 +0800

    side navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <nav class="fixed m-4 bg-white rounded-3xl shadow-xl w-[calc(100%-2rem)] bottom-0 border-2
+  <nav ref="nav" class="fixed m-4 bg-white rounded-3xl shadow-xl w-[calc(100%-2rem)] bottom-0 border-2
               md:flex md:w-20 md:h-[calc(100vh-2rem)] md:border-0">
     <div class="px-8 md:p-4 my-auto [&>div]:my-8 [&>div>a]:p-4 flex md:block justify-between">
       <div><nuxt-link to="/">H</nuxt-link></div>
@@ -9,11 +9,21 @@
     </div>
   </nav>
 
-  <div class="md:pl-28 pt-4">
+  <div class="md:pl-28 pt-4" :style="{'margin-bottom':navH+'px'}">
     <slot />
   </div>
 
 </template>
+
+<script setup>
+  import {onMounted} from "vue";
+
+  const nav = ref(0)
+  const navH = ref(0)
+  onMounted(() => {
+    navH.value = nav.value.clientHeight + 24
+  })
+</script>
 
 <style scoped>
 .router-link-exact-active{
