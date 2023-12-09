cad464fd8e1b788e9a2821bfde4f4b22cc95ca38
ekirian <ekidoseven@gmail.com>
2023-12-10 01:51:51 +0800

    clean up

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,45 @@
 
 <script lang="ts" setup>
 import { Line } from 'vue-chartjs';
-import {onMounted, ref} from "vue";
-import {DateTime} from 'luxon';
+import { DateTime } from 'luxon';
 import 'chartjs-adapter-moment';
-import {getRelativeTime} from "~/composables/getRelativeTime";
-import {toNumber} from "@vue/shared";
 
-interface ChartData {
-  x: number;
-  y: number;
-}
+// for refresh chart when change
+const chartLoaded = ref(false);
 
-interface Data {
-  [key: string]: {
-    time: number;
-    temp: number;
-    humid: number;
-  };
-}
+let temperature3HoursAgo = ref<number | null>(null); // Default to null or an initial value
 
-const timeArray: Date[] = [];
-const tempData: ChartData[] = [];
-const humidData: ChartData[] = [];
-
-const data = ref<Data>({});
-const loaded = ref(false);
-
-let convertedEpoch: Date | string ;
-
-const convertEpochTo = (epochTime: number, format: string): Date | string => {
-  switch (format) {
-    case 'JSDate':
-      convertedEpoch = DateTime.fromSeconds(epochTime).toJSDate();
-      break;
-    case 'ShortTime':
-      convertedEpoch = DateTime.fromSeconds(epochTime).toLocaleString({ timeStyle: 'short' });
-      break;
-  }
-  return convertedEpoch;
-};
-
-// const labels = months({ count: 7 });
-// console.log(labels)
+// data for chart
+const timeArrayForLabel: Date[] = [];
+const temperatureData: number[] = [];
+const humidityData: number[] = [];
 
 const chartData = ref({
-  labels: timeArray,
+  labels: timeArrayForLabel,
   datasets: [
+    // Temperature dataset
     {
       label: 'Temperature',
+      data: temperatureData,
       borderColor: `#da5b6a`,
       borderWidth: 2,
       radius: 0,
-      // tension: 0.2,
-      // stepped: true,
-      data: tempData,
       yAxisID: 'y',
     },
+    // Humidity dataset
     {
       label: 'Humidity',
+      data: humidityData,
       borderColor: `#b4ccef`,
       borderWidth: 2,
       radius: 0,
-      // tension: 0.2,
-      // stepped: true,
-      data: humidData,
       yAxisID: 'y1',
     },
   ],
 });
 
 const chartOptions = ref({
+  // Chart options configuration
   stacked: false,
   interaction: {
     mode: 'index',
@@ -86,7 +55,17 @@ const chartOptions = ref({
       type: 'time',
       time: {
         // Luxon format string
-        tooltipFormat: 'DD MMMM yyyy hh:mm a'
+        tooltipFormat: 'DD MMM yyyy hh:mm a',
+        displayFormats: {
+          day: 'DD MMM',
+          hour: 'hh:mm a',
+          minute: 'HH:mm',
+        }
+      },
+      ticks: {
+        autoSkip: true,
+        autoSkipPadding: 50,
+        maxRotation: 0
       },
     },
     y: {
@@ -119,120 +98,77 @@ const chartOptions = ref({
 });
 
 const selectedTimeRange = ref()
-const timeRanges = [{
-  name: '3 hours ago',
-  value: '3hours'
-}, {
-  name: 'Today',
-  value: 'today',
-}, {
-  name: '24 hours ago',
-  value: '24hours'
-},{
-  name: 'Yesterday',
-  value: 'yesterday'
-}, {
-  name: '3 days',
-  value: '3days'
-}]
+const dataCount = ref('select range first') // show data count for selected time range
 
-const lastTemperatureDifference = ref<string | null>(null); // Default to null or an initial value
+// variable for temperature card
+let lastUpdate: string,
+    temperature: number,
+    lastHumidity: number,
+    lastUpdateTime: string
 
-const getTemperatureDifferences = (): { time: Date; difference: number }[] => {
-  const differences: { time: Date; difference: number }[] = [];
-
-  if (tempData.length > 0) {
-    const lastTemp = tempData[tempData.length - 1];
-
-    // Filter data points from the last 3 hours
-    const last3HoursData = tempData.filter((_, index) => {
-      const timeDifference = timeArray[tempData.length - 1].getTime() - timeArray[index].getTime();
-      const hoursDifference = timeDifference / (1000 * 60 * 60);
-      return hoursDifference <= 3;
-    });
-
-    // Calculate differences
-    differences.push({ time: timeArray[tempData.length - 1], difference: 0 });
-
-    for (let i = 1; i < last3HoursData.length; i++) {
-      differences.push({
-        time: timeArray[tempData.length - i - 1],
-        difference: Number(lastTemp) - Number(last3HoursData[i]),
-      });
-    }
-  }
-
-  return differences;
-};
-
-
-const count = ref('select range first')
-const loading = ref(false)
-
-let lastUpdate:string,
-    temp:string,
-    lastUpdateTime:string,
-    lastHumidity:string = ""
-
+const timeRanges = [
+  { name: '3 hours ago',  value: '3hours'    },
+  { name: 'Today',        value: 'today'     },
+  { name: '24 hours ago', value: '24hours'   },
+  { name: 'Yesterday',    value: 'yesterday' },
+  { name: '3 days',       value: '3days'     },
+  { name: '7 days',       value: '7days'     }
+]
 
 onMounted(() => {
   selectedTimeRange.value = 'today'
 });
 
-// watch works directly on a ref
 watch(selectedTimeRange, async (timeRange) => {
-    loaded.value = false
-    count.value = 'calculating...'
+    chartLoaded.value = false
+    dataCount.value = 'calculating...'
 
     // Clear the arrays before fetching new data
-    timeArray.length = 0;
-    tempData.length = 0;
-    humidData.length = 0;
+    timeArrayForLabel.length = 0;
+    temperatureData.length = 0;
+    humidityData.length = 0;
 
     try {
-      const res = await fetch(`/api/temperature/query?timeRange=${timeRange}`)
+      const res = await fetch(`/api/temperature/test-query?timeRange=${timeRange}`)
+      // const res = await fetch(`/api/temperature/query?timeRange=${timeRange}`)
       const resData = await res.json()
-      const apiData = resData.data
-      count.value = resData.count
 
-      if (resData.count !== 0){
-        lastUpdate = getRelativeTime(resData.latest.time);
-        lastUpdateTime = convertEpochTo(resData.latest.time, 'ShortTime')
-        temp = resData.latest.temp;
-        lastHumidity = resData.latest.humid
-      }
+      dataCount.value = resData.count
 
-      for (const entry of apiData) {
-        // Assuming entry.time is already a number in the response
-        const timeLabel = convertEpochTo(entry.time, 'JSDate');
-        const timeDate = typeof timeLabel === 'string' ? new Date(timeLabel): timeLabel;
+      // get data for temperature card
+      if (resData.count !== 0) {
+        const latestData = resData.latest;
+        lastUpdate = getRelativeTime(latestData.time);
+        lastUpdateTime = DateTime.fromSeconds(latestData.time).toLocaleString({ timeStyle: 'short' });
+        temperature = latestData.temp;
+        lastHumidity = latestData.humid;
 
-        timeArray.push( timeDate );
-        tempData.push(entry.temp);
-        humidData.push(entry.humid);
-      }
-
-      // Calculate temperature differences
-      const temperatureDifferences = getTemperatureDifferences();
-
-      if (temperatureDifferences.length > 0) {
-        const lastTemperatureDifferenceValue = temperatureDifferences[temperatureDifferences.length - 2].difference;
-
-        // Format temperature with two decimal places
-        const formattedTemperatureDifference = Number(lastTemperatureDifferenceValue).toFixed(2);
-
-        // Update the ref with the formatted value
-        lastTemperatureDifference.value = formattedTemperatureDifference;
+        const indexToRetrieve = Math.max(resData.data.length - 35 - 1, 0);
+        temperature3HoursAgo = resData.data[indexToRetrieve]?.temp || 0;
       } else {
-        // Set the ref to null or an initial value if there are no differences
-        lastTemperatureDifference.value = null;
+        const lastData = await fetch(`/api/temperature/test-query?timeRange=last`);
+        const resDataLast = await lastData.json();
+
+        const latestData = resDataLast.latest;
+        lastUpdate = getRelativeTime(latestData.time);
+        lastUpdateTime = DateTime.fromSeconds(latestData.time).toLocaleString({ timeStyle: 'short' });
+        temperature = latestData.temp;
+        lastHumidity = latestData.humid;
+        temperature3HoursAgo = ref(0);
       }
+
+
+      resData.data.forEach((entry: { time: number, temp: number, humid: number }) => {
+        const timeLabel = DateTime.fromSeconds(entry.time).toJSDate();
+        timeArrayForLabel.push(timeLabel);
+        temperatureData.push(entry.temp);
+        humidityData.push(entry.humid);
+      });
 
     } catch (error) {
-      count.value = 'Error! Could not reach the API. ' + error
+      dataCount.value = 'Error! Could not reach the API. ' + error
     } finally {
-      loading.value = false
-      loaded.value = true
+      chartLoaded.value = true
     }
 })
 
@@ -242,16 +178,14 @@ watch(selectedTimeRange, async (timeRange) => {
 
 <template>
   <div class="px-4 md:px-0 mb-32">
-
-    <!--    content Wraper -->
     <div class="flex flex-col">
       <div class="md:flex flex-row md:space-x-4">
         <!--  Temperature Card-->
         <div class="md:basis-1/4 bg-white rounded-3xl overflow-hidden">
-          <temp-card :temp="temp"
+          <temp-card :temp="temperature"
                      :update="lastUpdate"
                      :time="lastUpdateTime"
-                     :change="lastTemperatureDifference "
+                     :change="(temperature - temperature3HoursAgo).toFixed(2)"
                      :humidity="lastHumidity"
                      high="34.1"
                      highTime="2m"
@@ -264,23 +198,27 @@ watch(selectedTimeRange, async (timeRange) => {
         <div class="md:basis-3/4 md:m-0 mt-4 p-4 bg-white rounded-3xl">
           <div class="flex justify-between mx-4">
             <h2 class="font-bold text-xl">Graph</h2>
-            <div class="flex"><span class="hidden md:block mr-2">Data Count:</span> {{ count }}</div>
-            <select v-model="selectedTimeRange">
-              <option v-for="timeRange in timeRanges" :value="timeRange.value">
-                {{timeRange.name}}
-              </option>
-            </select>
+            <div class="flex"><span class="hidden md:block mr-2">Data Count:</span> {{ dataCount }}</div>
 
+            <div class="relative inline-flex">
+              <select v-model="selectedTimeRange" class="appearance-none bg-white border border-gray-300 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:border-gray-500 focus:bg-white">
+                <option v-for="timeRange in timeRanges" :value="timeRange.value">
+                  {{timeRange.name}}
+                </option>
+              </select>
+              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <Icon name="iconamoon:arrow-down-2" />
+              </div>
+            </div>
           </div>
+
           <div class="h-48 md:h-80">
-            <Line v-if="loaded" :data="chartData" :options="chartOptions"/>
+            <Line v-if="chartLoaded" :data="chartData" :options="chartOptions"/>
           </div>
         </div>
-
-
-
       </div>
 
+      <!-- Calculation Card -->
       <div class="mt-4 p-4 bg-white rounded-3xl hidden md:block">
         <calculation-card/>
       </div>
